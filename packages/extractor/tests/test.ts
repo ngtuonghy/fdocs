7556fdd11346ec0cf460dd7e27a3c18f30cc5f51
ngtuonghy <ngtuonghy@gmail.com>
2024-09-29 16:54:50 +0700

    update test

--- packages/extractor/tests/test.ts
+++ packages/extractor/tests/test.ts
@@ -1,22 +1,24 @@
 import { bidvParser, vietinbankParser } from "../src/bank-statement-parser";
 import { vietcombankParser } from "../src/bank-statement-parser/vietnam/vietcombank";
 
-const run = async () => {
-	const b = await vietinbankParser("./vietinbank.pdf", {
-		outputFile: "vietinbank.csv",
-		format: "CSV",
-	});
-	// console.log(b);
+const extractor = async () => {
+	// const vietinbank = await vietinbankParser("./vietinbank.pdf", {
+	// 	format: "JSON",
+	// });
+	// console.log(vietinbank);
+	//
+	// const vietcombank = await vietcombankParser("./vietcombank.pdf", {
+	// 	outputFile: "vietcombank.csv",
+	// 	format: "CSV",
+	// });
+	// console.log(vietcombank);
 
-	const c = await vietcombankParser("./vietcombank14.pdf", {
-		outputFile: "vietcombank.csv",
-		format: "CSV",
-	});
-	// console.log(c);
-	const d = await bidvParser("./bidv.pdf", {
+	const bidv = await bidvParser("./bidv.pdf", {
+		pages: "all",
 		outputFile: "bidv.csv",
 		format: "CSV",
 	});
-	// console.log(d);
+
+	console.log(bidv);
 };
-run();
+extractor();
